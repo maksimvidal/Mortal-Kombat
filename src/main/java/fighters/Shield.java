28172f6e9de6796f4d6d3db426c83f4f7e598fe7
maksimvidal <silchenkko08@gmail.com>
2021-10-03 16:38:55 +0300

    Shield was added

---
+++ src/main/java/fighters/Shield.java
@@ -0,0 +1,14 @@
+package fighters;
+
+public class Shield extends Weapon {
+	private static final double DEFAULT_ATTACK=-1.0;
+	private static final double DEFAULT_HEALTH=20.0;
+	private static final double DEFAULT_DEFENSE=2.0;
+	
+	
+	public Shield(){
+		setAttack(DEFAULT_ATTACK);
+		setDefense(DEFAULT_DEFENSE);
+		setHealth(DEFAULT_HEALTH);
+	}
+}
